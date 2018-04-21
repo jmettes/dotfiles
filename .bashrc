79aa9a1a48bb7e618eaf7a8f4b4a09a87e6fb9e9
Jonathan Mettes <jonathan@jmettes.com>
2018-04-21 23:23:33 +1000

    Update

--- .bashrc
+++ .bashrc
@@ -1,1 +1,11 @@
 alias config='git --git-dir=$HOME/.cfg --work-tree=$HOME'
+alias ls='ls -p --color=auto'
+
+export PATH=~/bin:$PATH
+
+source ~/.bash_profile
+
+# enable horizontal scrolling with shift + vertical scroll
+# https://askubuntu.com/questions/404737/horizontal-scrolling-in-firefox-to-shiftmouse-scroll-instead-of-back-forward
+# also needs xbindkeys and xautomation installed
+xbindkeys
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,6 +2,7 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Util.Run (spawnPipe)
@@ -12,25 +13,26 @@ main :: IO ()
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ def
-        { terminal = "xterm -sl 1000"
+        { terminal = "urxvt"
         , manageHook = manageDocks <+> manageHook def
         , layoutHook = avoidStruts $ smartBorders $ layoutHook def
         , startupHook = setWMName "LG3D" -- java swing
-        , handleEventHook = handleEventHook def <+> docksEventHook
+        , handleEventHook = handleEventHook def <+> docksEventHook <+> fullscreenEventHook
         , logHook = dynamicLogWithPP xmobarPP
             { ppOutput = hPutStrLn xmproc
             , ppTitle = xmobarColor "grey" "" . shorten 53
             }
         , focusedBorderColor = "#2b6651"
         , normalBorderColor = "#1d1f21"
+        , modMask = mod4Mask
+        , focusFollowsMouse = False
         } `additionalKeys`
         [
-        ((mod1Mask, xK_p), spawn "GDK_DPI_SCALE=1.5 dmenu_run -fn inconsolata-18")
-        , ((mod1Mask, xK_apostrophe), sendMessage ToggleStruts)
+        ((mod4Mask, xK_p), spawn "GDK_DPI_SCALE=1.5 dmenu_run -fn inconsolata-18")
+        , ((mod4Mask, xK_apostrophe), sendMessage ToggleStruts)
         , ((0, xF86XK_AudioMute), spawn "amixer -q set Master toggle")
         , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -q set Master 5+")
         , ((0, xF86XK_AudioLowerVolume), spawn "amixer -q set Master 5-")
         , ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 10")
         , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10")
         ]
-
